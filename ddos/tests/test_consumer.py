274d63ae02dcf4ccc1a116b27b29e378b7d7449a
Erik <eirikval@gmail.com>
2020-02-08 16:54:45 -0600

    Ran test coverage report (~50%) and wrote first tests after spiking a bit

---
+++ ddos/tests/test_consumer.py
@@ -0,0 +1,22 @@
+from ..consumer import ApacheLogRecord, parse_apache_log_record
+
+
+bytes_apache_record = b'200.4.91.190 - - [25/May/2015:23:11:15 +0000] "GET / HTTP/1.0" 200 3557 "-" "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)"'
+
+
+def test_parse_apache_log_record_returns_namedtuple_of_nine_elements():
+    expected_response = ApacheLogRecord(
+        ip_address="200.4.91.190", 
+        client_id="-", 
+        user_id="-", 
+        timestamp="25/May/2015:23:11:15 +0000", 
+        request="GET / HTTP/1.0", 
+        response_status_code="200", 
+        response_size="3557",
+        referer_request_header="-", 
+        user_agent_request_header="Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)"
+    )
+    assert parse_apache_log_record(bytes_apache_record) == expected_response
+
+if __name__ == "__main__":
+    pass
