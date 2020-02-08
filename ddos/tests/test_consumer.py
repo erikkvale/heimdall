ad315830eddf93858dcd73613ce42c8f8a408632
Erik <eirikval@gmail.com>
2020-02-08 17:07:55 -0600

    Added test for custom parser exception being raised on tuple unpacking

--- ddos/tests/test_consumer.py
+++ ddos/tests/test_consumer.py
@@ -1,4 +1,5 @@
-from ..consumer import ApacheLogRecord, parse_apache_log_record
+import pytest
+from ..consumer import ApacheParserException, ApacheLogRecord, parse_apache_log_record
 
 
 bytes_apache_record = b'200.4.91.190 - - [25/May/2015:23:11:15 +0000] "GET / HTTP/1.0" 200 3557 "-" "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)"'
@@ -17,6 +18,12 @@ def test_parse_apache_log_record_returns_namedtuple_of_nine_elements():
         user_agent_request_header="Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)"
     )
     assert parse_apache_log_record(bytes_apache_record) == expected_response
+
+
+def test_parse_apache_log_record_raises_exception_if_tuple_unpacking_fails():
+    with pytest.raises(ApacheParserException):
+        parse_apache_log_record(b'200.4.91.190 - - [25/May/2015:23:11:15 +0000]')
+
 
 if __name__ == "__main__":
     pass
