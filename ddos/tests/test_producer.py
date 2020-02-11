2d9eb5953adee06fe702e317838e5693d45864c0
Erik <eirikval@gmail.com>
2020-02-11 06:15:29 -0600

    Updated call args after refactor

--- ddos/tests/test_producer.py
+++ ddos/tests/test_producer.py
@@ -3,12 +3,12 @@ from unittest import mock
 from ddos.producer import publish_log_record, apache_log_reader
 
 
-@mock.patch("ddos.producer.KafkaProducer.send")
+@mock.patch("ddos.producer.KafkaProducer")
 def test_publish_log_record(mock_producer):
     topic = "test"
     msg = '200.4.91.190 - - [25/May/2015:23:11:15 +0000] "GET / HTTP/1.0" 200 3557 "-" "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)"'
-    publish_log_record(topic, msg)
-    mock_producer.assert_called_with(topic, str.encode(msg))
+    publish_log_record(mock_producer, topic, msg)
+    mock_producer.send.assert_called_with(topic, str.encode(msg))
 
 
 def test_apache_log_reader(tmp_path):
