fdd568e8dfe45b9285645b0e53c64df460f27724
Erik <eirikval@gmail.com>
2020-02-11 07:23:17 -0600

    Removed main script statement

--- config/settings.py
+++ config/settings.py
@@ -8,7 +8,3 @@ load_dotenv(dotenv_path=env_path)
 KAFKA_TOPIC = os.getenv("KAFKA_TOPIC")
 KAFKA_BOOTSTRAP_SERVER = os.getenv("KAFKA_BOOTSTRAP_SERVER")
 APACHE_ACCESS_LOG_FILE_PATH = os.getenv("APACHE_ACCESS_LOG_FILE_PATH")
-
-if __name__ == "__main__":
-    print(env_path)
-
